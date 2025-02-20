b7726114e35245333989a77480ae27e5895c17b0
Rita Wanjiku <mwangirita@gmail.com>
2025-02-20 12:04:34 +0300

    Changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -13,7 +13,7 @@ CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
-    print DOUBLE NOT NULL,
+    price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE SET NULL
 );
