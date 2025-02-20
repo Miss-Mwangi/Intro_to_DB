f71867fef4a2d5df8681db3f412d9ec4b317420b
Rita Wanjiku <mwangirita@gmail.com>
2025-02-20 12:49:03 +0300

    Changes in MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
@@ -23,6 +23,7 @@ def create_database():
         if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
+            print("MySQL connection closed.")
 
 if __name__ == "__main__":
     create_database()
